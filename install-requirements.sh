23973f842f012b93788d60e9c33e2f7b89ff7c4e
EnergyFussl <lukas@friedl.li>
2017-11-21 08:08:20 +0000

    Updated Install Requirements

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,4 +1,4 @@
-apt-get update && apt-get install htop iftop sudo nano python python3
+apt-get update && apt-get upgrade && apt-get install htop iftop sudo nano python python3 python3-pymysql
 sudo apt-get install libglib2.0-0 libglib2.0-dev
 sudo apt-get install libdbus-1-dev 
 sudo apt-get install libudev-dev 
@@ -7,3 +7,4 @@ sudo apt-get install libreadline-dev
 sudo apt-get install libusb-1.0-0-dev
 wget http://www.kernel.org/pub/linux/bluetooth/bluez-5.32.tar.xz
 tar -xf bluez-5.32.tar.xz
+
