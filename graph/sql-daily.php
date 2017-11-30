16d635dc70b8f59ea3a22d2cb72c368e702b2f11
Lukas Friedl <lukas@friedl.li>
2017-11-30 20:05:18 +0100

    Signed-off-by: Lukas Friedl <lukas@friedl.li>

---
+++ graph/sql-daily.php
@@ -0,0 +1,34 @@
+<?php
+$servername = "localhost";
+$username = "lfrie_sensoren";
+$password = "dIea?163";
+$dbname = "lfriedl_sensoren";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT Day(datum) AS `Tag`, ROUND(AVG(temp),2) AS `Temperatur` FROM klima group by Day(datum)";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $data = array();
+        foreach ($result as $row) {
+                $data[] = $row;
+        }
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+$myJSON = json_encode($data);
+echo $myJSON;
+
+
+?>
